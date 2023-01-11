8631959523eea48eedd29905e510913df2a3e4a8
taureau75 <vixidev99@protonmail.com>
2023-01-10 16:58:24 -0800

    some progress on locking wbtc

---
+++ frontend/helpers/constants.js
@@ -0,0 +1,4 @@
+module.exports = {
+  MAINNET_RPC_URL: 'https://mainnet.infura.io/v3/3c7f5083c38943ea95aa49278ddeba53',
+  GOERLI_RPC_URL: 'https://goerli.infura.io/v3/3c7f5083c38943ea95aa49278ddeba53'
+}

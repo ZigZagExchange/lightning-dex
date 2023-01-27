9846a8a520702382c12cbc776ca01ad84f4b0788
taureau75 <vixidev99@protonmail.com>
2023-01-27 14:57:21 -0800

    simplify backend

--- backend/__tests__/api.js
+++ backend/__tests__/api.js
@@ -53,32 +53,14 @@ describe("Tests", () => {
     const encodedInvoice = LNInvoice.encode(invoice, privKey);
     const response = await request(app).post("/invoice").send({ invoice: encodedInvoice });
     expect(response.statusCode).toBe(500);
-    expect(response.body.err).toBe("Bad expiry. Expiry should be 1 hour");
+    expect(response.body.err).toBe("Bad expiry. Expires in less than 10 min");
   });
 
-  test("Get a list of invoices", async () => {
-    const response = await request(app).get("/invoices");
+  test("Get an invoice by hash", async () => {
+    const response = await request(app).get("/hash/" + hash);
     expect(response.statusCode).toBe(200);
-    expect(response.body instanceof Array).toBe(true);
-    expect(response.body.length).toBe(1);
-    expect(response.body[0].hash).toBe(hash);
-  });
-
-  test("Submit a preimage for an invoice", async () => {
-    const response = await request(app).post("/hash/preimage").send({ hash, preimage });
-    expect(response.statusCode).toBe(200);
-  });
-
-  test("Invoice info should include the preimage", async () => {
-    const response = await request(app).get("/invoices");
-    expect(response.body[0].preimage).toBe(preimage);
-  });
-
-  test("Submit a preimage without an invoice", async () => {
-    const preimage = "362f4499fd0466e03b603e43127b1d66";
-    const hash = crypto.createHash("sha256").update(preimage, "hex").digest("hex");
-    const response = await request(app).post("/hash/preimage").send({ hash, preimage });
-    expect(response.statusCode).toBe(200);
+    expect(response.body.hash).toBe(hash);
+    expect(response.body.invoice).toBeTruthy();
   });
 
   test("publish channels", async () => {
--- backend/app.js
+++ backend/app.js
@@ -16,9 +16,7 @@ async function runDbMigration () {
   await db.query(`
     CREATE TABLE IF NOT EXISTS hashes (
       hash TEXT PRIMARY KEY,
-      invoice TEXT,
-      preimage TEXT,
-      expiry INTEGER
+      invoice TEXT
     )
   `);
 }
@@ -46,14 +44,14 @@ app.post('/invoice', async (req, res, next) => {
   } catch (e) {
     return next("Bad invoice: " + e.message);
   }
-  const now = parseInt(Date.now() / 1000)
+  const now = Math.floor(Date.now() / 1000)
   const unix_expiry = decodedInvoice.timestamp + decodedInvoice.expiry;
   if (unix_expiry < now + 600) {
-    return next("Bad expiry. Expiry should be 1 hour");
+    return next("Bad expiry. Expires in less than 10 min");
   }
   const hash = decodedInvoice.paymentHash.toString('hex');
   try {
-    await db.query("INSERT INTO hashes (hash, invoice, expiry) VALUES ($1, $2, $3)", [hash, invoice, unix_expiry]);
+    await db.query("INSERT INTO hashes VALUES ($1, $2)", [hash, invoice]);
   }
   catch (e) {
     return next(e.detail);
@@ -61,25 +59,11 @@ app.post('/invoice', async (req, res, next) => {
   res.status(200).json({"success": true });
 })
 
-app.get('/invoices', async (req, res) => {
-  const invoices = await db.query("SELECT * FROM hashes WHERE invoice IS NOT NULL AND expiry > EXTRACT(EPOCH FROM NOW())");
-  res.status(200).json(invoices.rows);
-});
-
 app.get('/hash/:hash', async (req, res, next) => {
-  const hashes = await db.query("SELECT * FROM hashes WHERE hash=$1", [payment_hash]);
+  const hashes = await db.query("SELECT * FROM hashes WHERE hash=$1", [req.params.hash]);
   if (hashes.rows.length > 0) res.status(200).json(hashes.rows[0]);
   else next("Hash not found");
 })
-
-app.post('/hash/preimage', async (req, res, next) => {
-  const preimage = req.body.preimage;
-  const hash = req.body.hash;
-  const computedHash = crypto.createHash('sha256').update(preimage, 'hex').digest('hex');
-  if (hash !== computedHash) return next("preimage does not match hash");
-  await db.query("INSERT INTO hashes (hash, preimage) VALUES ($1,$2) ON CONFLICT (hash) DO UPDATE SET preimage=$2", [hash, preimage]);
-  res.status(200).json({"success": true });
-});
 
 let CHANNELS = [];
 app.post('/channels', async (req, res, next) => {

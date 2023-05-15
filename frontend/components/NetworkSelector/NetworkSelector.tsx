cf340db4b1c1e98e158f15993d28b8f7967e9631
magicdev090 <uearngold@outlook.com>
2023-05-15 23:01:32 +0300

    updated tokens

--- frontend/components/NetworkSelector/NetworkSelector.tsx
+++ frontend/components/NetworkSelector/NetworkSelector.tsx
@@ -1,4 +1,5 @@
-import { useContext } from "react"
+import { useContext, useEffect, useState } from "react"
+import { useAtom } from "jotai/react"
 
 import Jazzicon, { jsNumberForAddress } from "react-jazzicon"
 
@@ -8,8 +9,18 @@ import styles from "./NetworkSelector.module.css"
 import DownArrow from "../DownArrow"
 import { hideAddress } from "../../utils/utils"
 
+import { originTokenAtom } from "../../store/token"
+import { networksItems } from "../../utils/data"
+
 function NetworkSelector({ networkSelectorModalOpen }: { networkSelectorModalOpen: () => void }) {
   const { userAddress, username, network, connect, disconnect } = useContext(WalletContext)
+  const [originToken, setOriginToken] = useAtom(originTokenAtom)
+  const [tokenIcon, setTokenIcon] = useState<string>("")
+
+  useEffect(() => {
+    let icon = networksItems.filter(item => item.name === originToken)[0].icon
+    setTokenIcon(icon)
+  }, [originToken])
 
   function open() {
     networkSelectorModalOpen()
@@ -19,7 +30,10 @@ function NetworkSelector({ networkSelectorModalOpen }: { networkSelectorModalOpe
     return (
       <div className={`lg:flex hidden ${styles.container}`}>
         <button className={styles.connect_button} onClick={open}>
-          <div className={styles.text}>0.0 ETH</div>
+          <div className={styles.text}>
+            0.0 ETH
+            <Image src={`/tokenIcons/${tokenIcon}`} alt="Switch Network" className="w-5 h-5 ml-2 rounded-full" width={20} height={20} />
+          </div>
         </button>
       </div>
     )
@@ -46,5 +60,7 @@ function NetworkSelector({ networkSelectorModalOpen }: { networkSelectorModalOpe
     )
   }
 }
+import { useAmp } from "next/amp"
+import Image from "next/image"
 
 export default NetworkSelector
---
+++ frontend/utils/data.tsx
@@ -0,0 +1,155 @@
+export const networksItems1 = [
+    {
+        name: "Ethereum",
+        layer: "Layer 1",
+        color: "#5170ad",
+        icon: "eth.svg"
+    },
+    {
+        name: "Arbitrum",
+        layer: "Layer 2",
+        color: "#58535B",
+        icon: "abt.jfif"
+    },
+    {
+        name: "Avalanche",
+        layer: "Layer 1",
+        color: "#ef4444",
+        icon: "avax.svg"
+    },
+    {
+        name: "BNB Chain",
+        layer: "Layer 1",
+        color: "#ecae0b",
+        icon: "bnb.svg"
+    },
+    {
+        name: "Optimism",
+        layer: "Layer 2",
+        color: "#ef4444",
+        icon: "opt.png"
+    },
+    {
+        name: "Polygon",
+        layer: "Layer 2",
+        color: "#a855f7",
+        icon: "pol.jfif"
+    },
+    {
+        name: "Aurora",
+        layer: "Layer 1",
+        color: "#84cc16",
+        icon: "aur.png"
+    },
+    {
+        name: "Boba Network",
+        layer: "Layer 2",
+        color: "#84cc16",
+        icon: "bob.png"
+    },
+    {
+        name: "Canto",
+        layer: "Layer 1",
+        color: "#14b8a6",
+        icon: "canto.svg"
+    },
+    {
+        name: "Cronos",
+        layer: "Layer 1",
+        color: "#3b82f6",
+        icon: "cronos.png"
+    },
+    {
+        name: "DFK Chain",
+        layer: "Layer 1",
+        color: "#14b8a6",
+        icon: "dfk.png"
+    },
+    {
+        name: "Canto",
+        layer: "Layer 1",
+        color: "#14b8a6",
+        icon: "canto.svg"
+    },
+    {
+        name: "Dogechain",
+        layer: "Layer 1",
+        color: "#a855f7",
+        icon: "dog.png"
+    },
+    {
+        name: "Fantom",
+        layer: "Layer 1",
+        color: "#3b82f6",
+        icon: "fantom.jfif"
+    },
+    {
+        name: "Harmony",
+        layer: "Layer 1",
+        color: "#06b6d4",
+        icon: "harmony.jfif "
+    },
+    {
+        name: "Klaytn",
+        layer: "Layer 1",
+        color: "#f97316",
+        icon: "klaytn.jfif"
+    },
+    {
+        name: "Metis",
+        layer: "Layer 2",
+        color: "#14b8a6",
+        icon: "metis.png"
+    },
+    {
+        name: "Moonbeam",
+        layer: "Layer 1",
+        color: "#14b8a6",
+        icon: "moonbeam.jfif"
+    },
+    {
+        name: "Moonriver",
+        layer: "Layer 1",
+        color: "#a855f7",
+        icon: "moonriver.jfif"
+    },
+    {
+        name: "Terra",
+        layer: "Layer 1",
+        color: "#3b82f6",
+        icon: "terra.png"
+    },
+]
+
+export const networksItems = [
+    {
+        name: "Ethereum",
+        layer: "Layer 1",
+        color: "#5170ad",
+        icon: "eth.svg"
+    },
+    {
+        name: "Arbitrum",
+        layer: "Layer 2",
+        color: "#58535B",
+        icon: "abt.jfif"
+    },
+    {
+        name: "Solana",
+        layer: "Layer 1",
+        color: "#58535B",
+        icon: "sol.svg"
+    },
+    {
+        name: "Bitcoin",
+        layer: "Layer 1",
+        color: "#58535B",
+        icon: "btc.svg"
+    },
+    {
+        name: "Lightning",
+        layer: "Layer 2",
+        color: "#58535B",
+        icon: "ln.jfif"
+    },
+]

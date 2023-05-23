a1c2c20523bc712dc4467e9080edde2cd04203cb
taureau75 <vixidev99@protonmail.com>
2023-05-23 14:18:23 -0400

    grab last 100 blocks of deposits

--- scripts/eth_deposit.js
+++ scripts/eth_deposit.js
@@ -8,8 +8,8 @@ const bridgeAbi = JSON.parse(fs.readFileSync('BTCBridge.json', "utf-8")).abi;
 
 const ethersProvider = new ethers.providers.InfuraProvider("goerli", "3c7f5083c38943ea95aa49278ddeba53");
 
-const BRIDGE_ADDRESS = "0x1e3795cB5bAf44b23aBAF988c0fb77ed21CE6346";
+const BRIDGE_ADDRESS = "0x428Fe4d080A62127E6Bf167E05b05cF30079d3f2";
 const Bridge = new ethers.Contract(BRIDGE_ADDRESS, bridgeAbi, ethersProvider);
 
-const events = await Bridge.queryFilter(Bridge.filters.Deposit(), 0, "latest");
+const events = await Bridge.queryFilter(Bridge.filters.Deposit(), -100, "latest");
 console.log(events);

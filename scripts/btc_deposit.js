ba459cc375d2e4592378e74f80bbd578b4d513e0
taureau75 <vixidev99@protonmail.com>
2023-05-24 16:27:10 -0400

    working btc and eth deposits

--- scripts/btc_deposit.js
+++ scripts/btc_deposit.js
@@ -20,10 +20,19 @@ async function updateDeposits () {
   const transactions = JSON.parse(stdout);
   const deposits = transactions.filter(t => t.category === "receive");
   for (let deposit of deposits) {
-    console.log(deposit);
+    const deposit_address_result = await db.query("SELECT * FROM deposit_addresses WHERE deposit_address=$1 AND deposit_currency='BTC'", [deposit.address]);
+    if (deposit_address_result.rows.length != 1) continue;
+
+    const outgoing_currency = deposit_address_result.rows[0].outgoing_currency;
+    const outgoing_address = deposit_address_result.rows[0].outgoing_address;
+
+    const bridge_data = ["BTC", deposit.address, deposit.amount, deposit.txid, outgoing_currency, outgoing_address];
     const result = await db.query(
-      "INSERT INTO bridges (deposit_currency, deposit_address, deposit_amount, deposit_txid, deposit_timestamp) VALUES ($1,$2,$3,$4,NOW()) ON CONFLICT (deposit_txid) DO NOTHING", 
-      ["BTC", deposit.address, deposit.amount, deposit.txid]
+      `INSERT INTO bridges (deposit_currency, deposit_address, deposit_amount, deposit_txid, deposit_timestamp, outgoing_currency, outgoing_address) 
+       VALUES ($1,$2,$3,$4,NOW(),$5,$6) 
+       ON CONFLICT (deposit_txid) DO NOTHING`, 
+       bridge_data
     );
+    if (result.rowCount === 1) console.log(bridge_data);
   }
 }
--- scripts/eth_deposit.js
+++ scripts/eth_deposit.js
@@ -1,9 +1,17 @@
 import { ethers } from 'ethers'
 import dotenv from 'dotenv'
 import fs from 'fs'
-import { db } from 'db'
+import pg from 'pg'
 
 dotenv.config()
+
+const db = new pg.Pool({
+  host: 'localhost',
+  port: 5432,
+  database: 'zap',
+  user: 'postgres',
+  password: 'postgres'
+});
 
 const bridgeAbi = JSON.parse(fs.readFileSync('BTCBridge.json', "utf-8")).abi;
 
@@ -12,5 +20,24 @@ const ethersProvider = new ethers.providers.InfuraProvider("goerli", "3c7f5083c3
 const BRIDGE_ADDRESS = "0x428Fe4d080A62127E6Bf167E05b05cF30079d3f2";
 const Bridge = new ethers.Contract(BRIDGE_ADDRESS, bridgeAbi, ethersProvider);
 
-const events = await Bridge.queryFilter(Bridge.filters.Deposit(), -100, "latest");
-console.log(events);
+updateBridges();
+setInterval(updateBridges, 5000);
+
+async function updateBridges() {
+  const deposits = await Bridge.queryFilter(Bridge.filters.Deposit(), 0, "latest");
+
+  for (let deposit of deposits) {
+    let deposit_currency;
+    if (deposit.args.token === ethers.constants.AddressZero) deposit_currency = "ETH";
+    const deposit_amount = (deposit.args.amount.toString() / 1e18).toString();
+    const bridge_data = [deposit_currency, deposit.args.initiator, deposit_amount, deposit.transactionHash, deposit.args.out_chain, deposit.args.out_address];
+    const result = await db.query(
+      `INSERT INTO bridges (deposit_currency, deposit_address, deposit_amount, deposit_txid, deposit_timestamp, outgoing_currency, outgoing_address) 
+       VALUES ($1,$2,$3,$4,NOW(),$5,$6) 
+       ON CONFLICT (deposit_txid) DO NOTHING`, 
+       bridge_data
+    );
+    if (result.rowCount === 1) console.log(bridge_data);
+  }
+
+}

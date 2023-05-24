927ec0e64eeb130f4762608b1d56641b98555f98
taureau75 <vixidev99@protonmail.com>
2023-05-24 16:02:20 -0400

    deposit address schema

--- schema.sql
+++ schema.sql
@@ -1,3 +1,11 @@
+CREATE TABLE IF NOT EXISTS deposit_addresses (
+  deposit_currency TEXT,
+  deposit_address TEXT,
+  outgoing_currency TEXT,
+  outgoing_address TEXT
+);
+CREATE UNIQUE INDEX unique_deposit_address ON deposit_addresses(deposit_currency, outgoing_currency, outgoing_address);
+
 CREATE TABLE IF NOT EXISTS bridges(
     deposit_currency TEXT NOT NULL,
     deposit_address TEXT NOT NULL,

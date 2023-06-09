89ff8d9ab5bc8000f0bd9b5fd0f202f2d38659c4
taureau75 <vixidev99@protonmail.com>
2023-06-08 21:20:39 -0400

    move README to root

--- contracts/scripts/deploy.ts
+++ contracts/scripts/deploy.ts
@@ -1,8 +1,9 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  //const beneficiary = "0x6f457Ce670D18FF8bda00E1B5D9654833e7D91BB"; // goerli
-  const beneficiary = "0x98d066538842d2585fd26d0CF01C0f24e8eD716f"; // mainnet
+  const beneficiary = "0x26AFeEBc8012FD47521c955cfAd6d6172B06516C"; // ETH-SOL goerli
+  //const beneficiary = "0x6f457Ce670D18FF8bda00E1B5D9654833e7D91BB"; // ETH-BTC goerli
+  //const beneficiary = "0x98d066538842d2585fd26d0CF01C0f24e8eD716f"; // ETH-BTC mainnet
   const Bridge = await ethers.getContractFactory("BTCBridge");
   const bridge = await Bridge.deploy(beneficiary);
 

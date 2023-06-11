386a70f4d67c5f4bd08e3cfa478a8a00c3008300
taureau75 <vixidev99@protonmail.com>
2023-06-11 02:04:13 -0400

    build fixed

--- frontend/contexts/WalletContext.tsx
+++ frontend/contexts/WalletContext.tsx
@@ -60,10 +60,10 @@ export const WalletContext = createContext<WalletContextType>({
 function WalletProvider({ children }: Props) {
   const previousWagmiConnection = useAccount()
   const [chain, setChain] = useState<Chain>(getLocalStorage('chain', Chain.evm))
-  const [address, setAddress] = useState<string>(previousWagmiConnection.address)
+  const [address, setAddress] = useState<string>(previousWagmiConnection.address ? previousWagmiConnection.address : "")
   const [balance, setBalance] = useState('0.00 ETH')
   const [isLoading, setIsLoading] = useState(false)
-  const [isConnected, setIsConnected] = useState<Connected>(previousWagmiConnection.isConnected)
+  const [isConnected, setIsConnected] = useState<Connected>(previousWagmiConnection.isConnected ? "MataMask" : null)
   const [currentAction, setCurrentAction] = useState<CurrentAction>('Origin')
   const [orgChainId, setOrgChainId] = useState(Number(getLocalStorage('orgChainId', 1)))
   const [destChainId, setDestChainId] = useState(Number(getLocalStorage('destChainId', 42161)))

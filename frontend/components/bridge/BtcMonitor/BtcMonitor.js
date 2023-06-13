90d4ef49da5e0160876aa73364c69b7ffbb3b652
d7o1d1s0 <d7sarmy@gmail.com>
2023-06-13 14:35:32 +0100

    poll api for new incoming txs to deposit address

--- frontend/components/bridge/BtcMonitor/BtcMonitor.js
+++ frontend/components/bridge/BtcMonitor/BtcMonitor.js
@@ -2,14 +2,27 @@ import { useState, useEffect } from "react"
 
 const BtcMonitor = ({ btcDepositAddress, btcHeight }) => {
 
+    const [tx, setTx] = useState("")
+
     useEffect(() => {
         if (btcDepositAddress) {
             pollAPI(btcDepositAddress)
         }
     }, [btcDepositAddress])
 
+    function checkTransactions(transactions, btcHeight) {
+        for (let i = 0; i < transactions.length; i++) {
+            const tx = transactions[i];
+            if (tx.status && tx.status.block_height >= btcHeight) {
+                setTx(tx.txid)
+                return tx.txid
+            }
+        }
+    }
+
+
     async function pollAPI(btcDepositAddress) {
-        // let intervalId = setInterval(() => {
+        let intervalId = setInterval(async() => {
 
         await fetch('/api/btcAddressTxs', {
             method: 'POST',
@@ -20,16 +33,17 @@ const BtcMonitor = ({ btcDepositAddress, btcHeight }) => {
         })
             .then(r => r.json())
             .then(data => {
-                console.log(data)
+                if (checkTransactions(data)) {
+                    clearInterval(intervalId) // stop polling
+                }
             })
             .catch(error => console.error(error))
-        // }, 15000)
+        }, 15000)
     }
 
     return (<>
         <p>BtcMonitor</p>
-        <p>{btcDepositAddress}</p>
-        <p>{btcHeight}</p>
+        {tx ? <p>Pending tx: {tx}</p> : <p>Awaiting btc deposit...</p> }
     </>
     );
 };

fb4e9249bf50458d06a274ae9914289ad89c2d53
taureau75 <vixidev99@protonmail.com>
2023-06-13 23:32:08 -0400

    bug fix for phantom freeze

--- frontend/hooks/usePhantom.ts
+++ frontend/hooks/usePhantom.ts
@@ -4,17 +4,16 @@ const usePhantom = () => {
     const [phantomProvider, setPhantomProvider] = useState<any>(null)
 
     useEffect(() => {
-        console.log(window.phantom)
         // @ts-ignore
         if (typeof window.phantom?.solana !== 'undefined') {
             // @ts-ignore
             const provider = window.phantom?.solana
 
-            if (provider?.isPhantom) {
+            if (provider?.isPhantom && !phantomProvider) {
                 setPhantomProvider(provider)
             }
         }
-    }, [])
+    })
 
     return { phantomProvider }
 }

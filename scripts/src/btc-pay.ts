cca79d3d7773c006f4883a914002cffb5a7ba813
Ubuntu <ubuntu@ip-172-31-49-55.us-west-2.compute.internal>
2023-08-09 15:07:30 +0000

    btc bug fix

--- scripts/src/btc-pay.ts
+++ scripts/src/btc-pay.ts
@@ -8,7 +8,7 @@ const SCRIPT_INTERVAL = 10000
 const exec = util.promisify(nodeChildProcess.exec);
 
 const runScript = scriptWrapper(async ({db}) => {
-  const {rows: bridges} = await db.query("SELECT * FROM bridges WHERE paid = false AND outgoing_currency = BTC AND outgoing_address IS NOT NULL")
+  const {rows: bridges} = await db.query("SELECT * FROM bridges WHERE paid = false AND outgoing_currency = 'BTC' AND outgoing_address IS NOT NULL")
 
   if (bridges.length === 0) {
     return

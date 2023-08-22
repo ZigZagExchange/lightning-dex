4f605ae400fc7c8e86c9c4efffab358991a7b39b
taureau75 <vixidev99@protonmail.com>
2023-08-22 09:08:23 -0400

    zksync era details

--- contracts/deploy/deploy-zksync-era.ts
+++ contracts/deploy/deploy-zksync-era.ts
@@ -3,7 +3,8 @@ import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { Deployer } from "@matterlabs/hardhat-zksync-deploy";
 
 export default async function (hre: HardhatRuntimeEnvironment) {
-  const beneficiary = ""; // wallet address
+  //const beneficiary = ""; // wallet address
+  const beneficiary = "0x9c84e4Ea84DeFEe45FF6Ec64ea3b1D85C7B9a50E"; // mainnet
 
   const wallet = new Wallet("");
 
